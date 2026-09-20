cef6b4171f88bad212d11ea8c61c217d5c080ac0
novoselovegor994-beep <novoselovegor994@gmail.com>
2026-09-20 23:47:45 +0400

    2лаба

---
+++ labawinforms2/labawinforms2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace labawinforms2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(798, 334);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Нарисовать звезды";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(165, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 33);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Начать схождение";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(97, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Пауза";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 379);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 27);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Продолжить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(318, 340);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 33);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Расхождение";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(660, 340);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(566, 348);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Скорость звезд";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(716, 383);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(32, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(636, 388);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "кол-во точек:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 433);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ labawinforms2/labawinforms2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labawinforms2
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> points = new List<Point>();
+        
+        private List<Star> stars = new List<Star>();
+
+        private Random random = new Random();
+
+        private Timer animTimer = new Timer();
+
+        private float t = 0f;
+
+        private bool converting = true;
+
+        public Form1()   
+        {
+            InitializeComponent();
+            pictureBox1.Paint += pictureBox1_Paint;
+
+            // настройка таймера
+            animTimer.Interval = 16;
+            animTimer.Tick += AnimTimer_Tick;
+
+            // trackbar
+            trackBar1.Minimum = 1;
+            trackBar1.Maximum = 20;
+            trackBar1.Value = 5;
+
+            // кнопки
+            button2.Click += button2_Click;
+            button3.Click += button3_Click;
+            button4.Click += button4_Click;
+            button5.Click += button5_Click;
+
+
+
+
+        }
+        public class Star
+        {
+            public float StartX;
+            public float StartY;
+            public float X;
+            public float Y;
+            public float CenterX;
+            public float CenterY;
+
+            public Star(float x, float y, float cx, float cy)
+            {
+                StartX = x; StartY = y;
+                X = x; Y = y;
+                CenterX = cx; CenterY = cy;
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int count) || count <= 0)
+            {
+                MessageBox.Show("Введите положительное целое число!");
+                return;
+            }
+            
+            points.Clear();
+            stars.Clear();
+            t = 0f;
+            animTimer.Stop();
+
+            int w = pictureBox1.Width; // размеры picturebox
+            int h = pictureBox1.Height;
+            float cx = w / 2f;
+            float cy = h / 2f;
+
+            for (int i = 0; i < count; i++)
+            {
+                int x = random.Next(0, w);
+                int y = random.Next(0, h);
+                points.Add(new Point(x, y));
+                stars.Add(new Star(x, y, cx, cy));
+            }
+            pictureBox1.Invalidate();
+           
+
+        }
+
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Point p in points)
+            {
+                e.Graphics.FillEllipse(Brushes.Red, p.X - 2, p.Y - 2, 4, 4);
+            }
+            foreach (Star s in stars)
+            {
+                e.Graphics.FillEllipse(Brushes.White, s.X - 2, s.Y - 2, 4, 4);
+            } 
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (stars.Count == 0) return;
+            converting = true;
+            if (!animTimer.Enabled) animTimer.Start();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (stars.Count == 0) return;
+            if (!animTimer.Enabled) animTimer.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (stars.Count == 0) return;
+            converting = false;
+            if (!animTimer.Enabled) animTimer.Start();
+        }
+        private void AnimTimer_Tick(object sender, EventArgs e)
+        {
+            float speed = trackBar1.Value / 200f;
+            if (converting)
+            {
+                t += speed;
+                if (t >= 1f) { t = 1f; animTimer.Stop(); }
+            }
+            else
+            {
+                t -= speed;
+                if (t <= 0f) { t = 0f; animTimer.Stop(); }
+            }
+            foreach ( Star s in stars)
+            {
+                s.X = s.StartX + (s.CenterX - s.StartX) * t;
+                s.Y = s.StartY + (s.CenterY - s.StartY) * t;
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            animTimer.Stop();
+        }
+    }
+}
